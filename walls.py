87707c1ba2c9a861b1bd54d3d09dfd4e6c76e6b4
MiguelTeixeira07 <miguelcasacateixeira@gmail.com>
2026-04-24 01:05:21 +0100

    walls enum showcase

--- walls.py
+++ walls.py
@@ -1,50 +1,44 @@
-class Walls():
-
-    # 0 -> 0000
-    def empty():
-        print('  ')
-
-    # 1 -> 0001
-    def top():
-        print('═\n\n')
-
-    # 2 -> 0010
-    def right():
-        print('  ║')
-
-    # 3 -> 0011
-    def right_top_corner():
-        print(' ╗')
-
-    # 4 -> 0100
-    def bottom():
-        print('\n\n═')
-
-    # 5 -> 0101
-    def top_bottom():
-        print('═\n\n═')
-
-    # 6 -> 0110
-    def right_bottom_corner():
-        print(' ╝')
-
-    # 7 -> 0111
-    def top_right_bottom():
-        print('═╗\n ║\n═╝')
-
-    # 9 -> 1001
-    def left_top_corner():
-        print('╔ ')
-
-    # 12 -> 1100
-    def left_bottom_corner():
-        print('╚ ')
-
-    def vertical_line():
-        print(' ║ ')
-
-    def horizontal_line():
-        print(' ═ ')
+from enum import Enum
 
 
-Walls.top_right_bottom()
+class Walls(Enum):
+    EMPTY = ' '
+    TOP_LEFT = '╔'
+    H_LINE = '═'
+    TOP_RIGHT = '╗'
+    V_LINE = '║'
+    BOTTOM_LEFT = '╚'
+    BOTTOM_RIGHT = '╝'
+    TOP_JUNC = '╦'
+    RIGHT_JUNC = '╣'
+    LEFT_JUNC = '╠'
+    BOTTOM_JUNC = '╩'
+    ALL_JUNC = '╬'
+
+    def __str__(self):
+        return self.value
+
+
+print(Walls.TOP_LEFT, Walls.H_LINE, Walls.TOP_RIGHT, sep='')
+print(Walls.V_LINE, ' ', Walls.V_LINE, sep='')
+print(Walls.BOTTOM_LEFT, Walls.H_LINE, Walls.BOTTOM_RIGHT, sep='')
+
+print('\n')
+
+print(Walls.TOP_LEFT, Walls.H_LINE, Walls.TOP_JUNC, sep='', end='')
+print(Walls.H_LINE, Walls.TOP_RIGHT, sep='')
+print(Walls.V_LINE, ' ', Walls.V_LINE, ' ', Walls.V_LINE, sep='')
+print(Walls.BOTTOM_LEFT, Walls.H_LINE, Walls.BOTTOM_JUNC, sep='', end='')
+print(Walls.H_LINE, Walls.BOTTOM_RIGHT, sep='')
+
+print('\n')
+
+print(Walls.TOP_LEFT, Walls.H_LINE, Walls.TOP_JUNC, sep='', end='')
+print(Walls.H_LINE, Walls.TOP_RIGHT, sep='')
+print(Walls.V_LINE, ' ', Walls.V_LINE, ' ', Walls.V_LINE, sep='')
+print(Walls.LEFT_JUNC, Walls.H_LINE, Walls.ALL_JUNC, sep='', end='')
+print(Walls.H_LINE, Walls.RIGHT_JUNC, sep='')
+
+print(Walls.V_LINE, ' ', Walls.V_LINE, ' ', Walls.V_LINE, sep='')
+print(Walls.BOTTOM_LEFT, Walls.H_LINE, Walls.BOTTOM_JUNC, sep='', end='')
+print(Walls.H_LINE, Walls.BOTTOM_RIGHT, sep='')
