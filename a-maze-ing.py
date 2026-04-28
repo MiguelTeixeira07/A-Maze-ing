a5cfb78d828d84813b2181ca8b7c0f82269070c5
MiguelTeixeira07 <miguelcasacateixeira@gmail.com>
2026-04-28 16:51:46 +0100

    maze solving (limit is 40x40 mazes)

--- a-maze-ing.py
+++ a-maze-ing.py
@@ -1,7 +1,10 @@
 import sys
 from typing import Any
-from maze import Maze as mg
+from maze import Maze
 from input_parser import get_flags
+from colorama import init
+from solution import solve
+
 from display.display import print_maze
 
 
@@ -12,12 +15,18 @@ def main() -> None:
         print(f'Invalid syntax on {sys.argv[1]}')
         return
 
-    maze: mg = mg(flags['width'], flags['height'], flags['entry'], flags['exit'])
-    path = 'SSSSSSSSSSSSSSSSSSSEEEEEEEEEEEEEE'
+    maze: Maze = Maze(
+        flags['width'],
+        flags['height'],
+        flags['entry'],
+        flags['exit']
+    )
     maze.gen_dfs()
+    path = solve(maze)
     maze.output(flags['output_file'], flags['entry'], flags['exit'], path)
 
     print_maze(maze, flags['width'], flags['height'])
+    print(path)
 
 
 if __name__ == '__main__':
---
+++ solution.py
@@ -0,0 +1,33 @@
+from maze import Maze
+
+
+def solve(maze: Maze) -> str:
+    for row in maze.grid:
+        for cell in row:
+            cell.visited = False
+
+    def flood_fill(cell: Maze.Cell, turns: str) -> str:
+        if cell.exit:
+            return turns
+        
+        cell.visited = True
+        
+        if not cell.walls['North'] and not maze.grid[cell.y - 1][cell.x].visited:
+            result = flood_fill(maze.grid[cell.y - 1][cell.x], turns + 'N')
+            if result: return result
+        
+        if not cell.walls['East'] and not maze.grid[cell.y][cell.x + 1].visited:
+            result = flood_fill(maze.grid[cell.y][cell.x + 1], turns + 'E')
+            if result: return result
+        
+        if not cell.walls['South'] and not maze.grid[cell.y + 1][cell.x].visited:
+            result = flood_fill(maze.grid[cell.y + 1][cell.x], turns + 'S')
+            if result: return result
+        
+        if not cell.walls['West'] and not maze.grid[cell.y][cell.x - 1].visited:
+            result = flood_fill(maze.grid[cell.y][cell.x - 1], turns + 'W')
+            if result: return result
+
+        return ''
+
+    return flood_fill(maze.start, '')
