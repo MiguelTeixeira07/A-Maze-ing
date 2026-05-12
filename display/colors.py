55d39bf5ae9eb0d1095fadb7deeb5b0f54a071e2
MiguelTeixeira07 <miguelcasacateixeira@gmail.com>
2026-05-12 17:13:55 +0100

    idk man im just tired... changed some things, figure it out

--- display/colors.py
+++ display/colors.py
@@ -2,16 +2,17 @@ from random import choice
 from colorama import Fore
 
 
-def random_color(*exclude) -> str:
-    colors = [
-        Fore.RED,
-        Fore.BLUE,
-        Fore.CYAN,
-        Fore.GREEN,
-        Fore.BLACK,
-        Fore.YELLOW,
-        Fore.MAGENTA
-    ]
+COLORS = [
+    Fore.RED,
+    Fore.BLUE,
+    Fore.CYAN,
+    Fore.GREEN,
+    Fore.BLACK,
+    Fore.YELLOW,
+    Fore.MAGENTA
+]
 
-    filtered_colors = [color for color in colors if color not in exclude]
+def random_color(*exclude: str) -> str:
+
+    filtered_colors = [color for color in COLORS if color not in exclude]
     return choice(filtered_colors)
